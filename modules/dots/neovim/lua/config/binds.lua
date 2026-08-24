66f5150d5990f02e235e752bdba94b35cedf5710
4jamesccraven <4jamesccraven@gmail.com>
2026-08-24 17:55:52 -0400

    change(nvim): separate generic config into multiple files

---
+++ modules/dots/neovim/lua/config/binds.lua
@@ -0,0 +1,24 @@
+local map = require 'config.map'
+
+-- Leader Key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable mouse
+vim.opt.mouse = 'a'
+--> Keymap Configuration <--
+--
+-- Split navigation
+map('n', '<leader>vv', ':vsplit<CR>')
+map('n', '<leader>vs', ':split<CR>')
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
+-- Sorting
+map('v', '<C-s>', ':sort<CR>')
+-- You know what I meant
+map('n', 'q:', ':q<CR>')
