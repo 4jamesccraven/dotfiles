57179669522b6a3d962a13805bd955d5395869e3
4jamesccraven <4jamesccraven@gmail.com>
2026-09-12 13:11:45 -0400

    chore: update catppuccin overlay

--- overlay/drv/catppuccin-yazi.nix
+++ overlay/drv/catppuccin-yazi.nix
@@ -46,8 +46,8 @@ lib.checkListOfEnum "${pname}: variants" validVariants variants lib.checkListOfE
     src = pkgs.fetchFromGitHub {
       owner = "catppuccin";
       repo = "yazi";
-      rev = "fc69d6472d29b823c4980d23186c9c120a0ad32c";
-      hash = "sha256-Og33IGS9pTim6LEH33CO102wpGnPomiperFbqfgrJjw=";
+      rev = "d62802be39210ea10e54b3e3b09735c6cb9e57c1";
+      hash = "sha256-bwzEO8exoBwa19q+jnYjHkaamGl2mhfukIEhDfUCRGI=";
     };
 
     postInstall = ''
